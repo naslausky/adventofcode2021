9d38ef8894368d6bd96c13e4c2a21842f19423af
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-12-11 13:53:36 -0300

    Adiciona resolução do décimo primeiro dia.

--- 11/11.py
+++ 11/11.py
@@ -1,4 +1,8 @@
-with open('smallinput.txt') as file:
+# Desafio do dia 11/12/2021:
+#a) Receber uma grid 10x10 e uma regra de incremento. Calcular quantas vezes os números passam de 10 ao longo de 100 passos.
+#b) Calcular o número de passos para que a grid esteja toda com os números iguais.
+
+with open('input.txt') as file:
 	linhas = file.read().splitlines()
 	grid = {} 
 	for indiceLinha, linha in enumerate(linhas):
@@ -6,34 +10,38 @@ with open('smallinput.txt') as file:
 			coordenada = (indiceLinha, indiceCaracter)
 			grid[coordenada] = int(caracter)
 
-quantidadeDeFlashes = 0
-
-def incrementarCoordenada(grid, coordenada):
-	grid[coordenada] += 1
-	if grid[coordenada] == 9:
+def verificarFlash(grid, coordenada):
+	if coordenada in coordenadasComFlash:
+		return # Essa coordenada já teve flash esse turno.
+	if grid[coordenada] >= 10:
+		coordenadasComFlash.add(coordenada)
 		for dx in range(-1,2):
 			for dy in range(-1,2):
 				if dx != 0 or dy != 0:
 					coordenadaAdjacente = (coordenada[0]+dx, coordenada[1]+dy)
 					if coordenadaAdjacente in grid:
-						incrementarCoordenada(grid, coordenadaAdjacente)
+						grid[coordenadaAdjacente] += 1		
+						verificarFlash(grid, coordenadaAdjacente)
 def imprimirGrid(grid):
-	for ix in range(10):
-		linha = [grid[(ix, iy)] for iy in range(10)]
+	nLinhas = int(len(grid)**0.5)
+	for ix in range(nLinhas):
+		linha = [grid[(ix, iy)] for iy in range(nLinhas)]
 		print(linha)
 
 numeroDeFlashes = 0
-for _ in range(100):
+numeroDePassos = 0
+while sum([valor for valor in grid.values()]):
+	coordenadasComFlash = set()
 	for coordenada in grid:
-		incrementarCoordenada(grid, coordenada)
-	#numeroDeFlashesDessePasso = sum([1 for valor in grid.values() if valor>=9])
+		grid[coordenada] += 1
 	for coordenada in grid:
-		if grid[coordenada] >= 9:
+		verificarFlash(grid, coordenada)
+	for coordenada in grid:
+		if grid[coordenada] > 9:
 			numeroDeFlashes += 1
 			grid[coordenada] = 0
-	imprimirGrid(grid)
-	input()
-	
-
-print(numeroDeFlashes)
+	numeroDePassos += 1
+	if numeroDePassos == 100:
+		print("Número de flashes após 100 passos:", numeroDeFlashes)
+print("Número de passos para os polvos sincronizarem:", numeroDePassos)
 
