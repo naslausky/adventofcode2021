bb29cc1d9656dc1dc0fb848efd5a8f4a1df11baa
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-12-12 12:25:50 -0300

    Junta a parte 1 e a parte 2 do décimo segundo dia.

--- 12/12.py
+++ 12/12.py
@@ -12,39 +12,27 @@ with open('input.txt') as file:
 		else:
 			conexoes[destino] = {origem}
 
-def numeroDeCaminhosAteOFinal(rotaAteAqui):
-	noAtual = rotaAteAqui[-1]
-	resposta = 0
-	for conexao in conexoes[noAtual]:
-		if conexao.islower() and conexao in rotaAteAqui:
-			continue
-		if conexao == 'end':
-			resposta+=1
-		else:
-			rotaAtualizada = rotaAteAqui+[conexao]
-			resposta += numeroDeCaminhosAteOFinal(rotaAtualizada)
-	return resposta
-
-def numeroDeCaminhosAteOFinalParte2(rotaAteAqui):
+def numeroDeCaminhosAteOFinal(rotaAteAqui, parte2 = False):
 	noAtual = rotaAteAqui[-1]
 	resposta = 0
 	cavernasPequenasAteAgora = [caverna for caverna in rotaAteAqui 
 					if caverna.islower()]
-	existeCavernaDuplicada = any(cavernasPequenasAteAgora.count(cave)>1 
+	existeCavernaDuplicada = any(cavernasPequenasAteAgora.count(cave)>1 # Parte 2: Verifica se alguma caverna pequena já foi passada duas vezes.
 					for cave in cavernasPequenasAteAgora)
-
 	for conexao in conexoes[noAtual]:
-		if conexao == 'start':
-			continue
 		if conexao == 'end':
-			#print(rotaAteAqui)
-			resposta+=1
+			resposta += 1
 			continue
-		if conexao.islower():
-			if existeCavernaDuplicada:
-				if conexao in rotaAteAqui:
-					continue
-		rotaAtualizada = rotaAteAqui+[conexao]
-		resposta += numeroDeCaminhosAteOFinalParte2(rotaAtualizada)
+		if parte2: # Uma conexão pequena pode ser passada duas vezes se outra já não tiver feito isso.
+			if conexao == 'start':
+				continue
+			if conexao.islower() and existeCavernaDuplicada and conexao in rotaAteAqui:
+				continue
+		elif conexao.islower() and conexao in rotaAteAqui: # Uma conexão pequena só pode ser passada uma vez na parte 1.
+			continue
+		rotaAtualizada = rotaAteAqui + [conexao]
+		resposta += numeroDeCaminhosAteOFinal(rotaAtualizada, parte2)
 	return resposta
-print(numeroDeCaminhosAteOFinalParte2(['start']))
+
+print('Número de formas de chegar ao final passando uma vez por cada caverna pequena:', numeroDeCaminhosAteOFinal(['start']))
+print('Número de formas de chegar ao final passando duas vezes em uma caverna pequena:', numeroDeCaminhosAteOFinal(['start'], True))
