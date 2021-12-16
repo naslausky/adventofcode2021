f3c81b18c42700b9ec28178d1c2aba25986581ec
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-12-15 23:52:25 -0300

    Segunda tentativa décimo quinto dia.

--- 15/15.py
+++ 15/15.py
@@ -1,41 +1,60 @@
 with open('input.txt') as file:
 	linhas = file.read().splitlines()
-	numeroDeLinhas = len(linhas)
-	numeroDeColunas = len(linhas[0])
-	mapa = {}
+	coordenadaFim = (len(linhas) - 1, len(linhas[0]) - 1 )
+	tamanhoOriginal = (len(linhas), len(linhas[0]))
+	mapa = {} # Dicionário com o valor de cada coordenada.
 	for indiceLinha, linha in enumerate(linhas):
 		for indiceNumero, numero in enumerate(linha):
 			coordenada = (indiceLinha, indiceNumero)
 			mapa[coordenada] = int(numero)
 
-menorResposta = None
-gabarito = {}
-def menorRiscoAteOFim(coordenadaAtual, riscoAteAqui):
-	global menorResposta
-	if menorResposta and menorResposta < riscoAteAqui:
-		return riscoAteAqui
-
-	parametros = (coordenadaAtual, riscoAteAqui)
-	if parametros in gabarito:
-		return gabarito[parametros]
-
-	if coordenadaAtual == (numeroDeLinhas-1, numeroDeColunas-1): #Já chegou
-		menorResposta = riscoAteAqui + mapa[coordenadaAtual]
-		return riscoAteAqui + mapa[coordenadaAtual]
-
-	caminhos = ((0,1),(1,0) )#  ,(-1,0),(0,-1))
-	riscosPossiveis = []
-	for caminho in caminhos:
+coordenadaAtual = (0,0) #Começa no começo.
+coordenadasVerificadas = set() # Dicionário que relaciona o menor valor possível para chegar em uma dada coordenada.
+gabarito = {coordenadaAtual : 0} # Dicionário que relaciona o mínimo possível para alcançar cada nó.
+while (coordenadaFim not in coordenadasVerificadas): #Todas as coordenadas precisam ser verificadas.
+	possibilidadesDeCaminhos = ((1,0),(0,1),(-1,0),(0,-1))
+	for caminho in possibilidadesDeCaminhos:
 		proximaCoordenada = (coordenadaAtual[0]+caminho[0], coordenadaAtual[1]+caminho[1])
-		if proximaCoordenada in mapa:
-			riscosPossiveis.append(menorRiscoAteOFim(proximaCoordenada,
-						 riscoAteAqui+mapa[proximaCoordenada]))
-	if riscosPossiveis:
-		resultado = min(riscosPossiveis) 
-		gabarito[parametros] = resultado
-		return resultado
-	else:
-		return None
-
-print(menorRiscoAteOFim((0,0),0))
-	
+		if proximaCoordenada not in mapa:
+			continue
+		gabarito[proximaCoordenada] = min(gabarito[coordenadaAtual] + mapa[proximaCoordenada],
+						 gabarito.get(proximaCoordenada, 1000))
+	coordenadasVerificadas.add(coordenadaAtual)
+	proximasCandidatas = [coordenada for coordenada in gabarito if coordenada not in coordenadasVerificadas]
+	if proximasCandidatas:
+		custoDaProximaCoordenadaAVerificar = min(gabarito[coordenada] for coordenada in proximasCandidatas)
+		proximasCoordenadaAVerificar = [coordenada for coordenada in proximasCandidatas if gabarito[coordenada]==custoDaProximaCoordenadaAVerificar]
+		coordenadaAtual = proximasCoordenadaAVerificar[0]
+print(gabarito[coordenadaFim])	
+# Parte 2:
+coordenadaFim = (((coordenadaFim[0]+1)*5)-1, ((coordenadaFim[1]+1)*5)-1)
+novoMapa = {}
+for chave in mapa:
+	for indiceColuna in range(5):
+		for indiceLinha in range(5):
+			coordenadaReplicada = (chave[0]+indiceLinha*tamanhoOriginal[0], chave[1] + indiceColuna*tamanhoOriginal[1])	
+			valor =  (mapa[chave] + indiceColuna + indiceLinha) % 9
+			valor = 9 if valor == 0 else valor
+			novoMapa[coordenadaReplicada] = valor
+#for indiceLinha in range(coordenadaFim[0]+1):
+#	numerosDessaLinha = [str(novoMapa[(indiceLinha, indiceColuna)]) for indiceColuna in range(coordenadaFim[1]+1)]
+#	print(''.join(numerosDessaLinha))
+mapa = novoMapa
+coordenadaAtual = (0,0) #Começa no começo.
+coordenadasVerificadas = set() # Dicionário que relaciona o menor valor possível para chegar em uma dada coordenada.
+gabarito = {coordenadaAtual : 0} # Dicionário que relaciona o mínimo possível para alcançar cada nó.
+while (coordenadaFim not in coordenadasVerificadas): #Todas as coordenadas precisam ser verificadas.
+	possibilidadesDeCaminhos = ((1,0),(0,1),(-1,0),(0,-1))
+	for caminho in possibilidadesDeCaminhos:
+		proximaCoordenada = (coordenadaAtual[0]+caminho[0], coordenadaAtual[1]+caminho[1])
+		if proximaCoordenada not in mapa:
+			continue
+		gabarito[proximaCoordenada] = min(gabarito[coordenadaAtual] + mapa[proximaCoordenada],
+						 gabarito.get(proximaCoordenada, 100000))
+	coordenadasVerificadas.add(coordenadaAtual)
+	proximasCandidatas = [coordenada for coordenada in gabarito if coordenada not in coordenadasVerificadas]
+	if proximasCandidatas:
+		custoDaProximaCoordenadaAVerificar = min(gabarito[coordenada] for coordenada in proximasCandidatas)
+		proximasCoordenadaAVerificar = [coordenada for coordenada in proximasCandidatas if gabarito[coordenada]==custoDaProximaCoordenadaAVerificar]
+		coordenadaAtual = proximasCoordenadaAVerificar[0]
+print(gabarito[coordenadaFim])	
