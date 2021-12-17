88cffd8947d077e97d392d2f09f31a87707a3e82
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-12-17 11:00:15 -0300

    Adiciona a resolução do décimo sétimo dia.

--- 17/17.py
+++ 17/17.py
@@ -1,45 +1,43 @@
+#Desafio do dia 17/12/2021:
+#a) Receber uma área destino e calcular a maior altura que um torpedo pode alcançar e ainda passar por ela.
+#b) Calcular quantas velocidades iniciais distintas passam pela área destino.
+
 with open('input.txt') as file:
 	linha = file.read().splitlines()[0]
-#	linha = 'target area: x=20..30, y=-10..-5'
 	linha = linha.replace('target area: ', '')
 	areaDestino = []
 	for stringIntervalo in linha.split(', '):
 		intervalo = stringIntervalo.split('=')[1].split('..')
 		intervalo = list(map(int,intervalo))
 		areaDestino.append(intervalo)
-#print(abs(areaDestino[1][1]-areaDestino[1][0]+1))
-#print(areaDestino[1][0])
-velocidadesIniciais = {} #Dicionário que relaciona as velocidades iniciais que alcançam o 
+velocidadesIniciais = {} # Dicionário que relaciona as velocidades iniciais que alcançam o destino com sua maior altura alcançada.
 for x0 in range(1,areaDestino[0][1]+1):
 	y0=areaDestino[1][0] 
-	while True: #Laco de incrementar o y0
+	while True: # Laco que incrementa a velocidade vertical inicial.
 		posicaoAtual = [0,0]
 		velocidade = [x0,y0]
 		maiorAltura = 0
 		estaNaAreaDestino = False
-		while (not estaNaAreaDestino): #Faz 1 passo:
-#			print('posicao', posicaoAtual)
-#			print('velocidade', velocidade)
-			for dim in range(2):
+		while (not estaNaAreaDestino): # Faz passo a passo:
+			for dim in range(2): # Incrementa a posição atual.
 				posicaoAtual[dim] += velocidade[dim]
 			maiorAltura = max(maiorAltura, posicaoAtual[1])
 
-			estaNaAreaDestino = True #Verifica se está na area destino:
+			estaNaAreaDestino = True # Verifica se está na area destino:
 			for dim in range(2):
 				if posicaoAtual[dim] not in range(areaDestino[dim][0],
 							areaDestino[dim][1]+1):
 					estaNaAreaDestino = False
 
-			velocidade[0] -= 1 if velocidade[0] != 0 else 0
-			velocidade[1] -= 1
-			#if abs(velocidade[1]) > abs(areaDestino[1][1]-areaDestino[1][0]+1) and velocidade[1]<0:
-			if posicaoAtual[1] < areaDestino[1][0]:
+			velocidade[0] -= 1 if velocidade[0] != 0 else 0 # Devido ao atrito com a água.
+			velocidade[1] -= 1 # Devido a gravidade.
+			if posicaoAtual[1] < areaDestino[1][0]: # Se já tá mais embaixo do que a área destino, podemos tentar outra velocidade inicial:
 				break
 
 		if estaNaAreaDestino:
 			velocidadesIniciais[(x0,y0)] = maiorAltura
 
-		if y0 > 300: # Melhorar limite superior
+		if y0 > 200: # Melhorar limite superior
 			break
 		y0+=1
 
