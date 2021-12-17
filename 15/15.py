2bb1bd59e2b66d8935150ab8d43879dde2540a82
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-12-17 11:29:31 -0300

    Adiciona a resolução do décimo quinto dia.

--- 15/15.py
+++ 15/15.py
@@ -1,3 +1,7 @@
+# Desafio do dia 15/12/2021:
+#a) Receber uma matriz de pesos e calcular o menor custo do caminho de um canto ao outro.
+#b) Idem, porém com a matriz de pesos aumentada em 5 vezes em cada dimensão.
+
 with open('input.txt') as file:
 	linhas = file.read().splitlines()
 	coordenadaFim = (len(linhas) - 1, len(linhas[0]) - 1 )
@@ -25,7 +29,7 @@ while (coordenadaFim not in coordenadasVerificadas): #Todas as coordenadas preci
 		custoDaProximaCoordenadaAVerificar = min(gabarito[coordenada] for coordenada in proximasCandidatas)
 		proximasCoordenadaAVerificar = [coordenada for coordenada in proximasCandidatas if gabarito[coordenada]==custoDaProximaCoordenadaAVerificar]
 		coordenadaAtual = proximasCoordenadaAVerificar[0]
-print(gabarito[coordenadaFim])	
+print('Menor custo para percorrer a caverna:', gabarito[coordenadaFim])	
 # Parte 2:
 coordenadaFim = (((coordenadaFim[0]+1)*5)-1, ((coordenadaFim[1]+1)*5)-1)
 novoMapa = {}
@@ -36,25 +40,24 @@ for chave in mapa:
 			valor =  (mapa[chave] + indiceColuna + indiceLinha) % 9
 			valor = 9 if valor == 0 else valor
 			novoMapa[coordenadaReplicada] = valor
-#for indiceLinha in range(coordenadaFim[0]+1):
-#	numerosDessaLinha = [str(novoMapa[(indiceLinha, indiceColuna)]) for indiceColuna in range(coordenadaFim[1]+1)]
-#	print(''.join(numerosDessaLinha))
 mapa = novoMapa
-coordenadaAtual = (0,0) #Começa no começo.
-coordenadasVerificadas = set() # Dicionário que relaciona o menor valor possível para chegar em uma dada coordenada.
-gabarito = {coordenadaAtual : 0} # Dicionário que relaciona o mínimo possível para alcançar cada nó.
-while (coordenadaFim not in coordenadasVerificadas): #Todas as coordenadas precisam ser verificadas.
-	possibilidadesDeCaminhos = ((1,0),(0,1),(-1,0),(0,-1))
-	for caminho in possibilidadesDeCaminhos:
-		proximaCoordenada = (coordenadaAtual[0]+caminho[0], coordenadaAtual[1]+caminho[1])
-		if proximaCoordenada not in mapa:
-			continue
-		gabarito[proximaCoordenada] = min(gabarito[coordenadaAtual] + mapa[proximaCoordenada],
-						 gabarito.get(proximaCoordenada, 100000))
-	coordenadasVerificadas.add(coordenadaAtual)
-	proximasCandidatas = [coordenada for coordenada in gabarito if coordenada not in coordenadasVerificadas]
-	if proximasCandidatas:
-		custoDaProximaCoordenadaAVerificar = min(gabarito[coordenada] for coordenada in proximasCandidatas)
-		proximasCoordenadaAVerificar = [coordenada for coordenada in proximasCandidatas if gabarito[coordenada]==custoDaProximaCoordenadaAVerificar]
-		coordenadaAtual = proximasCoordenadaAVerificar[0]
-print(gabarito[coordenadaFim])	
+gabarito = {coordenadaFim : 0} # Dicionário que relaciona o mínimo possível para alcançar cada nó.
+mudouAlgo = True
+while (mudouAlgo):
+	mudouAlgo = False
+	for indiceLinha in range(coordenadaFim[0],-1,-1):
+		for indiceColuna in range(coordenadaFim[1],-1,-1):
+			coordenadaAtual = (indiceLinha, indiceColuna)
+			possibilidadesDeCaminhos = ((1,0),(0,1),(-1,0),(0,-1))
+			valores = []
+			for caminho in possibilidadesDeCaminhos:
+				proximaCoordenada = (coordenadaAtual[0]+caminho[0], coordenadaAtual[1]+caminho[1])
+				if proximaCoordenada not in mapa:
+					continue
+				valores.append(mapa[proximaCoordenada] + gabarito.get(proximaCoordenada, 10**6))
+			valores.append(gabarito.get(coordenadaAtual, 10**6))
+			minimo = min(valores)
+			if minimo != gabarito.get(coordenadaAtual, 10**6):
+				mudouAlgo = True
+				gabarito[coordenadaAtual] = minimo
+print('Menor custo para percorrer a caverna expandida:', gabarito[(0,0)])	
