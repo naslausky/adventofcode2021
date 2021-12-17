1dd4a44bd011335adabcd4f71c171b338177fdd7
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-12-17 01:27:09 -0300

    Adiciona resolução do décimo sexto dia.

--- 16/16.py
+++ 16/16.py
@@ -1,83 +1,84 @@
+# Desafio do dia 16/12/2021:
+#a) Receber uma sequência de bits, e uma regra de protocolo, e somar o número de versão de todos pacotes e sub pacotes.
+#b) Interpretar o significado de cada pacote, podendo representar operações e calcular o valor do pacote mais externo.
+
 with open('input.txt') as file:
 	linha = file.read().splitlines()[0]
-	#linha = 'D2FE28' #primeiro caso
-	#linha = '38006F45291200'
-	#linha = 'EE00D40C823060'
-	#linha = '8A004A801A8002F478'
-	#linha = '620080001611562C8802118E34'
-	#linha = 'C0015000016115A2E0802F182340'
-	#linha = 'A0016C880162017C3686B18A3D4780'
-#	print(linha)
-	linha = '1'+linha # Gambiarra pra incluir os zeros iniciais.
+	linha = '1' + linha # Para incluir os zeros a esquerda, coloco um bit 1 na frente e depois removo ele.
 	numeroHexadecimal = int(linha,16)
-	stringEmBinario = bin(numeroHexadecimal)[3:] # Exclui o 0b e o 1 inicial que eu coloquei na gambiarra.
-	#while stringEmBinario[-1] == '0':
-	#	stringEmBinario = stringEmBinario[:-1] # Descarta os zeros ao final do pacote mais externo.
-	#print(stringEmBinario)
-	#print('00111000000000000110111101000101001010010001001000000000')
-	#input()	
+	stringEmBinario = bin(numeroHexadecimal)[3:] # Exclui o '0b' e o '1' inicial que eu coloquei acima.
 
-def gerarPacoteBaseadoNosBits(stringBinaria):
-	bitsVersao = stringBinaria[:3]
+def gerarPacoteBaseadoNosBits(stringBinaria): #Função recursiva que recebe uma string de bits e retorna:
+	# 1) Um dicionário representando o (primeiro) pacote formado por ela, incluindo os seus subPacotes.
+	# 2) O resto da string que não foi utilizado para gerar esse pacote.
+	bitsVersao = stringBinaria[:3] # Os três primeiros bits são o número da versão.
 	versaoDoPacote = int(bitsVersao, 2)
-	bitsTipo = stringBinaria[3:6]
+	bitsTipo = stringBinaria[3:6] # Os três bits seguintes representam o identificador de tipo.
 	idTipo = int(bitsTipo, 2)
-	dicPacote = {'versao': versaoDoPacote, 'idTipo': idTipo}
+	dicPacote = {'versao': versaoDoPacote, 'idTipo': idTipo} # Cria o dicionário final com o que já temos.
 	restoDaString = stringBinaria[6:]
-	#print('DicAtéAgora:', dicPacote)
-	if idTipo == 4: # Valor literal:
-	#	print('idTipo', idTipo)
+	if idTipo == 4: # Representa um valor literal:
 		numeroBinario = ''
-	#	input()
 		chegouNoUltimoPacote = False
 		while (not chegouNoUltimoPacote):
 			subPacote = restoDaString[:5]
 			restoDaString = restoDaString[5:]
-			if subPacote[0] == '0': #É o ultimo pacote
+			if subPacote[0] == '0': # É o ultimo pacote pois começa com 0.
 				chegouNoUltimoPacote = True
 			subPacote = subPacote[1:]
 			numeroBinario += subPacote
 		numeroLiteral = int(numeroBinario, 2)
-		dicPacote['valorLiteral'] = numeroLiteral
+		dicPacote['valor'] = numeroLiteral
 		return dicPacote, restoDaString
-	else: # Operador:
-		
-		idTipoComprimento = restoDaString[0]
+	else: # Representa um operador (e seus operandos): 
+		idTipoComprimento = restoDaString[0]  #Faço esse par de linhas muitas vezes, poderia ser movido para uma função.
 		restoDaString = restoDaString[1:]
-		dicPacote['idTipoComprimento'] =  int(idTipoComprimento)
-		if idTipoComprimento == '0':
-	#		print('entrou id comprimento 0')
+		dicPacote['idTipoComprimento'] =  int(idTipoComprimento) # Não precisa para resposta final.
+		if idTipoComprimento == '0': # Significa que os próximos 15 bits representam o número de bits de sub-pacotes.
 			comprimentoSubPacotes = restoDaString[:15]
 			restoDaString = restoDaString[15:]
 			comprimentoSubPacotes = int(comprimentoSubPacotes, 2)
-			
 			bitsSubPacotes = restoDaString[:comprimentoSubPacotes]
 			restoDaString = restoDaString[comprimentoSubPacotes:]
-
-			#if (bitsSubPacotes):
 			primeiroPacote, bitsSubPacotes = gerarPacoteBaseadoNosBits(bitsSubPacotes)
 			subPacotes = [primeiroPacote]
-			while bitsSubPacotes:
+			while bitsSubPacotes: # Monta próximos pacotes enquanto não foram utilizados todos os bits reservados.
 				proximoPacote, bitsSubPacotes = gerarPacoteBaseadoNosBits(bitsSubPacotes)
 				subPacotes.append(proximoPacote)
-			dicPacote['subPacotes'] = subPacotes
-			return dicPacote, restoDaString
-		else:
+		else: # Significa que os próximos 11 bits representam quantos sub-pacotes tem.
 			numeroSubPacotes = restoDaString[:11]
 			restoDaString = restoDaString[11:]
 			numeroSubPacotes = int(numeroSubPacotes, 2)
 			subPacotes = []
-			for _ in range(numeroSubPacotes):
+			for _ in range(numeroSubPacotes): # Monta pacotes pelo número de vezes informado.
 				subPacote, restoDaString = gerarPacoteBaseadoNosBits(restoDaString)
 				subPacotes.append(subPacote)
-			dicPacote['subPacotes'] = subPacotes
-			return dicPacote, restoDaString
+		dicPacote['subPacotes'] = subPacotes
+		# Parte 2:
+		if idTipo == 0: # Operador que soma os valores dos sub pacotes.
+			resultado = sum(subPacote['valor'] for subPacote in subPacotes)
+		elif idTipo == 1: # Operador que retorna o produto dos valores dos sub pacotes.
+			produto = 1
+			for subPacote in subPacotes:
+				produto *= subPacote['valor']
+			resultado = produto
+		elif idTipo == 2: # Operador que retorna o valor mínimo dos sub pacotes.
+			resultado = min(subPacote['valor'] for subPacote in subPacotes)
+		elif idTipo == 3: # Operador que retorna o valor máximo dos sub pacotes.
+			resultado = max(subPacote['valor'] for subPacote in subPacotes)
+		elif idTipo == 5: # Operador com dois sub pacotes que retorna 1 se o primeiro for maior, 0 caso contrário.
+			resultado = 1 if subPacotes[0]['valor'] > subPacotes[1]['valor'] else 0
+		elif idTipo == 6: # Operador com dois sub pacotes que retorna 1 se o primeiro for menor, 0 caso contrário.
+			resultado = 1 if subPacotes[0]['valor'] < subPacotes[1]['valor'] else 0
+		elif idTipo == 7: # Operador com dois sub pacotes que retorna 1 se o valor de ambos é igual, 0 caso contrário..
+			resultado = 1 if subPacotes[0]['valor'] == subPacotes[1]['valor'] else 0
+		dicPacote['valor'] = resultado 
+		return dicPacote, restoDaString
 
-pacotePrincipal, _ = gerarPacoteBaseadoNosBits(stringEmBinario)
+pacotePrincipal, _ = gerarPacoteBaseadoNosBits(stringEmBinario) # Monta o dicionário do pacote mais externo baseado no input.
 
-def calcularSomaVersao(pacote):
+def calcularSomaVersao(pacote): # Função recursiva que calcula a resposta para a parte 1:
 	return pacote['versao'] + sum(calcularSomaVersao(subPacote) for subPacote in pacote.get('subPacotes',[]))
 
-print(calcularSomaVersao(pacotePrincipal))
-
-
+print('Soma dos números de versão de todos os pacotes:', calcularSomaVersao(pacotePrincipal))
+print('Valor do pacote da camada mais externa:', pacotePrincipal['valor'])
