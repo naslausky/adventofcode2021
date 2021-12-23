ae468b62e685dece1d2ab95d389ae5487e66969f
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2021-12-23 00:11:41 -0300

    Termina de arrumar o décimo oitavo dia.

--- 18/18.py
+++ 18/18.py
@@ -7,7 +7,7 @@ import copy
 with open('input.txt') as file:
 	linhasString = file.read().splitlines()
 	linhas = list(map(eval,linhasString))
-	linhasOriginais = copy.deepcopy(linhas) # Para a parte 2
+	linhasOriginais = copy.deepcopy(linhas) # Para a parte 2.
 
 def explodirSeNecessario(parOriginal, profundidade): 
 	# Função recursiva, que recebe um par e explode uma vez apenas, se válido.
@@ -34,27 +34,22 @@ def explodirSeNecessario(parOriginal, profundidade):
 						if type(parOriginal[0]) is int :
 							parOriginal[0]+=esquerda
 						else:
-							incrementarADireita(parOriginal[0], esquerda)
+							incrementarIndice(parOriginal[0], esquerda, 1)
 						esquerda = None
 				if direita:
 					if indice == 0:
 						if type(parOriginal[1]) is int:
 							parOriginal[1] += direita
 						else:
-							incrementarAEsquerda(parOriginal[1], direita)
+							incrementarIndice(parOriginal[1], direita, 0)
 						direita = None
 				return esquerda, explodiu, direita
 	return None, False, None # Não explodiu nem há nada para somar.
-def incrementarADireita (par, numero): #Função que adiciona um número ao número mais a direita do par.
+def incrementarIndice (par, numero, indice): #Função que adiciona um número ao número mais a direita/esquerda..
 	parAtual = par
-	while type(parAtual[1]) is list:
-		parAtual = parAtual[1]
-	parAtual[1] += numero
-def incrementarAEsquerda (par, numero):
-	parAtual = par
-	while type(parAtual[0]) is list:
-		parAtual = parAtual[0]
-	parAtual[0]+= numero
+	while type(parAtual[indice]) is list:
+		parAtual = parAtual[indice]
+	parAtual[indice] += numero
 def dividirSeNecessario(parOriginal):
 	dividiu = False
 	for indice, elemento in enumerate(parOriginal):
@@ -71,48 +66,35 @@ def somar(atual, proximaLinha): #Função que junta dois pares em um novo par.
 	if not atual: #Só para o primeiro caso, quando o par anterior é vazio.
 		return proximaLinha
 	return [atual, proximaLinha]
-somaAteAgora = []
-for linha in linhas:
-	somaAteAgora = somar(somaAteAgora, linha)
+def reduzir(linhaAtual): # Função que reduz um par. Aplica explosões e divisões até não poder mais.
 	mudouAlgo = True
 	while mudouAlgo:
 		mudouAlgo = False
-		_, explodiu, _ = explodirSeNecessario(somaAteAgora, 0)
+		_, explodiu, _ = explodirSeNecessario(linhaAtual, 0)
 		if explodiu:
 			mudouAlgo = True
 			continue
-		dividiu = dividirSeNecessario(somaAteAgora)
+		dividiu = dividirSeNecessario(linhaAtual)
 		if dividiu:
 			mudouAlgo = True
 			continue
-def calcularMagnitude(par):
+def calcularMagnitude(par): # Função que recebe um par e calcula sua pontuação.
 	magnitude = 0
-	if type(par[0]) is int:
-		magnitude += 3 * par[0]
-	else:
-		magnitude += 3 * calcularMagnitude(par[0])
-	if type(par[1]) is int:
-		magnitude += 2 * par[1]
-	else:
-		magnitude += 2 * calcularMagnitude(par[1])
+	coordenadasValores = ((0,3),(1,2)) # Atribui um valor para cada coordenada.
+	for coordenada, valor in coordenadasValores:
+		if type(par[coordenada]) is int:
+			elemento = par[coordenada]
+		else:
+			elemento = calcularMagnitude(par[coordenada])
+		magnitude += valor * elemento
 	return magnitude
-
-#print('Testando soma de magnitude:')
-teste1 = [[1,2],[[3,4],5]]
-teste2 = [[[[0,7],4],[[7,8],[6,0]]],[8,1]]
-teste3 = [[[[1,1],[2,2]],[3,3]],[4,4]]
-teste4 = [[[[3,0],[5,3]],[4,4]],[5,5]]
-teste5 = [[[[5,0],[7,4]],[5,5]],[6,6]]
-teste6 = [[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]	
-testes = ((teste1,143),(teste2,1384),(teste3,445),(teste4,791),(teste5,1137),(teste6, 3488))
-for teste,gabarito in testes:
-	pass
-#	print('_______________')
-#	print('Resultado:', calcularMagnitude(teste))
-#	print('Gabarito: ', gabarito)
-print('Resposta final:', calcularMagnitude(somaAteAgora))
+# Parte 1:
+somaAteAgora = []
+for linha in linhas:
+	somaAteAgora = somar(somaAteAgora, linha)
+	reduzir(somaAteAgora)
+print('A magnitude da soma final é:', calcularMagnitude(somaAteAgora))
 # Parte 2:
-#print()
 linhas = linhasOriginais
 magnitudeMaxima = 0 
 for linha1Original in linhas:
@@ -122,47 +104,6 @@ for linha1Original in linhas:
 		if linha1 == linha2:
 			continue
 		soma = somar(linha1,linha2)
-		mudouAlgo = True
-		while mudouAlgo:
-			mudouAlgo = False
-			_, explodiu, _ = explodirSeNecessario(soma, 0)
-			if explodiu:
-				mudouAlgo = True
-				continue
-			dividiu = dividirSeNecessario(soma)
-			if dividiu:
-				mudouAlgo = True
-				continue
+		reduzir(soma)
 		magnitudeMaxima = max(magnitudeMaxima, calcularMagnitude(soma))
-print(magnitudeMaxima)
-#Testes para cada função:
-teste2 = [[[[[9,8],1],2],3],4]
-gabarito2 = [[[[0,9],2],3],4]
-teste3 = [7,[6,[5,[4,[3,2]]]]] 
-gabarito3 = [7,[6,[5,[7,0]]]]
-teste4 = [[6,[5,[4,[3,2]]]],1]
-gabarito4 = [[6,[5,[7,0]]],3]
-teste5 = [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
-gabarito5 = [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
-teste6 = [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
-gabarito6 = [[3,[2,[8,0]]],[9,[5,[7,0]]]]
-testes = ((teste2,gabarito2),(teste3,gabarito3),(teste4,gabarito4),(teste5,gabarito5),(teste6,gabarito6))
-#print('Testando explosão:')
-for teste,gabarito in testes: #testes de explodir
-#	print('_______________')
-#	print('Anterior: ', teste)
-	explodirSeNecessario(teste, 0)
-#	print('Resultado:', teste)
-#	print('Gabarito :', gabarito)
-#print('Testando divisão:')
-teste7 = [[[[0,7],4],[15,[0,13]]],[1,1]]
-gabarito7 = [[[[0,7],4],[[7,8],[0,13]]],[1,1]]
-teste8 = [[[[0,7],4],[[7,8],[0,13]]],[1,1]] 
-gabarito8 =[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]
-testes = ((teste7,gabarito7),(teste8,gabarito8))
-for teste,gabarito in testes: #Testes de dividir
-#	print('_______________')
-#	print('Anterior: ',teste)
-	dividirSeNecessario(teste)
-#	print('Resultado:', teste)
-#	print('Gabarito: ', gabarito)
+print('A maior magnitude obtida com a soma de dois elementos é:', magnitudeMaxima)
