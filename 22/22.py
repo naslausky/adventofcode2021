1f36d78107f38cbc2e8324d98f9e399a46194275
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2022-04-13 19:31:51 -0300

    Melhora os comentários do vigésimo segundo dia.

--- 22/22.py
+++ 22/22.py
@@ -18,7 +18,7 @@ with open('input.txt') as file:
 intervalosOrdenados = [[],[],[]] # Lista que pega todos os intervalos possíveis em cada dimensão. Cada instrução adiciona dois números em cada lista.
 for intervalos, _ in instrucoes:
 	for indice, intervalo in enumerate(intervalos):
-		intervaloNormalizado = (intervalo[0],intervalo[1]+1) # Soma um ao índice superior pois cada instrução acende os índices inferior e superior inclusive.
+		intervaloNormalizado = (intervalo[0],intervalo[1] + 1) # Soma um ao índice superior pois cada instrução acende os índices inferior e superior inclusive.
 		intervalosOrdenados[indice].extend(intervaloNormalizado)
 intervalosOrdenados = [list(set(r))for r in intervalosOrdenados] # Remove duplicatas na lista de intervalos.
 [r.sort() for r in intervalosOrdenados]
@@ -30,7 +30,7 @@ def intervaloEstaContidoNaInstrucao(intervalos, instrucao): # Função que verif
 	return True
 	
 def intervaloEstaContidoEmOutro(menor, maior): # Função que verifica se um intervalo (a,b) está incluso em outro.
-	if menor[0]>= maior[0] and menor[1] <= maior[1]+1: # +1 foi necessário pois "maior" é utilizado vindo direto das instruções de entrada.
+	if menor[0] >= maior[0] and menor[1] <= maior[1] + 1: # Somar 1 foi necessário pois "maior" é utilizado vindo direto das instruções de entrada.
 		return True
 	return False
 
@@ -43,19 +43,19 @@ def intervaloEstaContidoNaZonaDeInicializacao(intervalos): # Função que difere
 
 somaRegiaoInicializacao = 0 # Resposta parte 1.
 somaCompleta = 0 # Resposta parte 2.
-for indiceX in range(len(intervalosOrdenados[0])-1): # Verifica todas as combinações de intervalos possíveis.
-	rangeX = (intervalosOrdenados[0][indiceX],intervalosOrdenados[0][indiceX+1]) # Monta o intervalo a ser testado.
-	iX = [(i,l) for i, l in instrucoes if intervaloEstaContidoEmOutro(rangeX,i[0])] # Das instruções, quais afetam esse intervalo X específico.
-	if not iX: #
+for indiceX in range(len(intervalosOrdenados[0]) - 1): # Verifica todas as combinações de intervalos possíveis.
+	rangeX = (intervalosOrdenados[0][indiceX], intervalosOrdenados[0][indiceX+1]) # Monta o intervalo a ser testado.
+	iX = [(i, l) for i, l in instrucoes if intervaloEstaContidoEmOutro(rangeX, i[0])] # Das instruções, quais afetam esse intervalo X específico.
+	if not iX: # Se não existe nenhuma instrução que afete esse intervalo, não tem porque seguir com ela. Ex: Instruções (1, 2) e (3, 4) não afetam o intervalo (2,3).
 		continue
-	for indiceY in range(len(intervalosOrdenados[1])-1):
-		rangeY = (intervalosOrdenados[1][indiceY],intervalosOrdenados[1][indiceY+1])
-		iY = [(i,l) for i,l in iX if intervaloEstaContidoEmOutro(rangeY, i[1])] # Das instruções que sobraram, quais também afetam esse intervalo Y. 
+	for indiceY in range(len(intervalosOrdenados[1]) - 1):
+		rangeY = (intervalosOrdenados[1][indiceY], intervalosOrdenados[1][indiceY+1])
+		iY = [(i,l) for i, l in iX if intervaloEstaContidoEmOutro(rangeY, i[1])] # Das instruções que sobraram, quais também afetam esse intervalo Y. 
 		if not iY:
-			continue # Não tem porque iterar sobre todos os intervalos Z se nenhuma instrução afeta essa combinação de intervalos X e Y.
-		for indiceZ in range(len(intervalosOrdenados[2])-1):
-			rangeZ = (intervalosOrdenados[2][indiceZ],intervalosOrdenados[2][indiceZ+1])
-			iZ = [(i,l) for i,l in iY if intervaloEstaContidoEmOutro(rangeZ,i[2])]
+			continue # Como acima, não tem porque iterar sobre todos os intervalos Z se nenhuma instrução afeta essa combinação de intervalos X e Y.
+		for indiceZ in range(len(intervalosOrdenados[2]) - 1):
+			rangeZ = (intervalosOrdenados[2][indiceZ], intervalosOrdenados[2][indiceZ+1])
+			iZ = [(i, l) for i, l in iY if intervaloEstaContidoEmOutro(rangeZ, i[2])]
 			if not iZ:
 				continue
 			intervaloAceso = iZ[-1][1] # É necessário apenas olhar a última instrução que afeta esse intervalo X,Y,Z.
@@ -64,7 +64,7 @@ for indiceX in range(len(intervalosOrdenados[0])-1): # Verifica todas as combina
 									(rangeY[1] - rangeY[0]) *
 									(rangeX[1] - rangeX[0]))
 				somaCompleta += quantidadeDeElementosNoIntervalo
-				if intervaloEstaContidoNaZonaDeInicializacao((rangeX,rangeY,rangeZ)):
+				if intervaloEstaContidoNaZonaDeInicializacao((rangeX,rangeY,rangeZ)): # Soma separada para a primeira parte.
 					somaRegiaoInicializacao += quantidadeDeElementosNoIntervalo
 
 print('Quantidade de cubos acesos na região de inicialização:', somaRegiaoInicializacao)
