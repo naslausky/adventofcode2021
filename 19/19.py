0dfa76d224c2c27388aff365b1501e07c715c5ed
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2022-10-13 20:42:57 -0300

    Termina versão inicial do décimo nono dia. Funcionando para o input exemplo, porém errado para o verdadeiro.

--- 19/19.py
+++ 19/19.py
@@ -35,7 +35,7 @@ def permutacoesDeUmaTupla(tupla): #Retorna todas as 24 permutações dos eixos d
 			)
 
 listaScanners = [] # Lista em que cada elemento é uma lista de tuplas contendo a leitura de cada beacon.
-with open('smallinput.txt') as file:
+with open('input.txt') as file:
 	linhasBeacons = file.read().split('\n\n')
 	for linhaBeacon in linhasBeacons:
 		linhasDesteBeacon = linhaBeacon.splitlines()[1:]
@@ -43,40 +43,51 @@ with open('smallinput.txt') as file:
 							map(int,linha.split(','))) 
 							for linha in linhasDesteBeacon]
 		listaScanners.append(linhasDesteBeacon)	
-
+resposta = sum([len(x) for x in listaScanners])
+paresEncontrados = set()
 for indiceScanner in range(len(listaScanners)): # Para cada Scanner, procurar outro: 
-	for indiceScanner2 in range(indiceScanner, len(listaScanners)):
-		if indiceScanner == indiceScanner2:
-			continue
+	for indiceScanner2 in range(indiceScanner+1, len(listaScanners)):
+		parScanner = (indiceScanner, indiceScanner2)
 		listaScanner = listaScanners[indiceScanner]
 		listaSegundoScanner = listaScanners[indiceScanner2]
 		# Verifica se esses dois scanners tem no mínimo 12 em comum:
 		# Precisa fazer isso para cada uma das 24 permutações.
 		# Só permutamos o segundo até esse estar na mesma permutação do primeiro.
-		print('tentando scanners:', indiceScanner, indiceScanner2)
+		#print('Testando scanners:', indiceScanner, indiceScanner2)
 		l1 = listaScanner.copy()
 		l1.sort()
+		l2 = listaSegundoScanner.copy()
 		numeroDeBeaconsScanner1 = len(l1)
+		numeroDeBeaconsScanner2 = len(l2)
 		for indicePermutacao in range(24):
-			l2 = [permutacoesDeUmaTupla(x)[indicePermutacao] for x in listaSegundoScanner.copy()]
-			l2.sort()
-			numeroDeBeaconsScanner2 = len(l2)
+			l2P = [permutacoesDeUmaTupla(x)[indicePermutacao] for x in l2]
+			l2P.sort()
+			#if (indicePermutacao == 4 and indiceScanner == 0 and indiceScanner2 == 1):
+			#	[print(l1[x], '\t\t', l2[x]) for x in range(25)]
+			#	input()
 			for indicePivot1 in range(numeroDeBeaconsScanner1):
 				for indicePivot2 in range(numeroDeBeaconsScanner2):
+					if parScanner in paresEncontrados:
+						continue
 					px, py, pz = l1[indicePivot1] # Coordenadas dos pivots:
-					px2, py2, pz2 = l2[indicePivot2]
+					px2, py2, pz2 = l2P[indicePivot2]
 					#Cria novas listas relativas ao pivot especifico:
 					l1Relativo = [(x-px, y-py, z-pz) for x, y, z in l1]
-					l2Relativo = [(x-px, y-py, z-pz) for x, y, z in l2]
+					l2Relativo = [(x-px2, y-py2, z-pz2) for x, y, z in l2P]
 					# Se eles tiverem 12 elementos em comum, achou
-					if indicePermutacao == 0 and indicePivot1 == 9 and indicePivot2 == 0:
-						print('pivot1', px,py,pz)
-						print('pivot2', px2,py2,pz2)
-						print(l1Relativo)
-						print(l2Relativo)
-						input()
+					#if (indicePermutacao == 0 and indiceScanner == 0 and indiceScanner2 == 1 and 
+					#	indicePivot1 == 6 and indicePivot2 == 4):
+					#	print('pivot1', px,py,pz)
+					#	print('pivot2', px2,py2,pz2)
+					#	[print(l1Relativo[x], '\t\t', l2Relativo[x]) for x in range(25)]
+					#	input()
 					elementosEmComum = [x for x in l1Relativo if x in l2Relativo]
-					if indiceScanner == 0 and indiceScanner2 == 1 and len(elementosEmComum)!= 0:
-						print('achados na permutacao>', len(elementosEmComum), indicePermutacao)
-					if len(elementosEmComum) >= 12:
-						print('Achou!', indiceScanner, indiceScanner2)
+					if len(elementosEmComum)>=12:
+						print('Achado!: Scanner: ',indiceScanner,' Scanner2:', indiceScanner2,'Elementos em comum: ',
+							 len(elementosEmComum),'Permutação: ', indicePermutacao)
+						#[print(l1Relativo[x], l2Relativo[x]) for x in range(25)]
+						resposta-=len(elementosEmComum)
+						paresEncontrados.add(parScanner)
+
+
+print(resposta)
