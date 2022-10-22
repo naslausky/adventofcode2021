30dbba7903bf7d15a07ada6590ef1094466c0173
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2022-10-21 21:06:19 -0300

    Adiciona resolução final do décimo nono dia.

--- 19/19.py
+++ 19/19.py
@@ -8,6 +8,9 @@ def diminuiTuplas(x,y):
 	x1,x2,x3 = x
 	y1,y2,y3 = y
 	return (x1-y1, x2-y2, x3-y3)
+
+def comporFuncao(f1, f2):
+	return lambda x, y, z: f1 (*f2 (x,y,z))
 	
 def permutacoesDeUmaTupla(indice): #Retorna todas as 24 permutações dos eixos da coordenada tupla.
 	if indice == 0: return lambda x, y, z:(x, y, z)
@@ -61,8 +64,9 @@ def permutacoesInversasDeUmaTupla(indice):
 	if indice == 22: return lambda x, y, z: (-z, -y, -x)
 	if indice == 23: return lambda x, y, z: (-y, z, -x)
 
+
 listaScanners = [] # Lista em que cada elemento é uma lista de tuplas contendo a leitura de cada beacon.
-with open('smallinput.txt') as file:
+with open('input.txt') as file:
 	linhasBeacons = file.read().split('\n\n')
 	for linhaBeacon in linhasBeacons:
 		linhasDesteBeacon = linhaBeacon.splitlines()[1:]
@@ -123,7 +127,7 @@ for indiceScanner in range(len(listaScanners)): # Para cada Scanner, procurar ou
 						posicoesRelativas[parScanner] = posicaoRelativa
 						permutacoes[parScanner] = permutacoesInversasDeUmaTupla(indicePermutacao)
 						
-						
+						#Permutacao[(a,b)] representa a função que precisamos aplicar a A para coincidir com os elementos de B
 						#Fazer de novo mudando o referencial para o outro scanner:  Serve só pra facilitar a segunda parte.
 						#Para isso, primeiramente a distancia é invertida (a-b vira b-a), e em seguida transformamos usando a inversa
 						parScannerInverso = (indiceScanner2, indiceScanner) # Talvez usar o tuple(reversed())
@@ -132,20 +136,6 @@ for indiceScanner in range(len(listaScanners)): # Para cada Scanner, procurar ou
 						posicaoRelativaInversa = permutacoesInversasDeUmaTupla(indicePermutacao)(*posicaoRelativaInversa)
 						posicoesRelativas[parScannerInverso] = posicaoRelativaInversa
 
-
-
-
-#[print(x,y) for x,y in posicoesRelativas.items()]
-#[print(x,y) for x,y in permutacoes.items()]
-#print('_________')
-#[print(x,y) for x,y in permutacoes.items()]
-# Preciso preencher o permutações completamente antes de usar abaixo:
-
-#
-#permutacoes[(0,4)] = permutacoesInversasDeUmaTupla(4)
-#
-#input()
-#beaconsFinais = set()
 qtdScanners = len(listaScanners) 
 while ( len([x for x,y in posicoesRelativas.items() if x[0]==0] ) < qtdScanners):
 	caminhosDescobertosAteOZero = {x:y for x,y in posicoesRelativas.items() if x[0] == 0}
@@ -158,18 +148,37 @@ while ( len([x for x,y in posicoesRelativas.items() if x[0]==0] ) < qtdScanners)
 			if par[0] == destino and novoPar not in posicoesRelativas:
 				novosCaminhos[novoPar] = somaTuplas(
 												distancia,
-												permutacoes[(0,par[0])](*delta)
+												permutacoes[(par[0],0)](*delta)
 											)
-											
-				f1 = permutacoes[(0, par[0])]
-				f2 = permutacoes[par]
-				funcaoComposta = lambda x, y, z: f2(*f1 (x,y,z))
-				novasPermutacoes[par[1],0] = funcaoComposta #Como eu salvei ao contrario preciso salvar o contrario do contrario
-				
+				#Eu tinha o 0 a X e de X a Y
+				# permutação [0, Y] é a função que deve ser aplicada aos elementos de 0 para ficarem iguais aos vistos por Y
+				# ou seja,
+				# Se Y enxerga (1,2,3), aplicamos permutacao(Y,X), significa que o 1,2,3 está permutado corretamente para a visão do X
+				# Com o resultado disso aplicamos permutacao(X,0), significa que o ponto que antigamente Y via agora está para o 0.
+				# Como fizemos um ponto visto por Y convertido para ser visto por 0, significa que isso é o valor de permutacao [Y,0]
 				f1 = permutacoes[(par[0], 0)]
-				f2 = permutacoes[par[1],par[0]]
-				funcaoComposta = lambda x, y, z: f1 (*f2 (x,y,z))
-				novasPermutacoes[novoPar] = funcaoComposta
+				f2 = permutacoes[(par[1],par[0])]
+				funcaoComposta = comporFuncao(f1,f2) 
+				novasPermutacoes[par[1],0] = funcaoComposta
+				#print('_______COMPONDO FUNCOES______')
+				#print ('f1(1,2,3):', f1(1,2,3), 'par',(0,par[0]) )
+				#print ('f2(1,2,3):', f2(1,2,3), 'par',par )
+				#print ('fc(1,2,3):', funcaoComposta(1,2,3), 'par',(par[1],0) )
+				#print('_____________________________')
+				
+				# Se 0 encherga (1,2,3), aplicamos a permutacao (0,X), que é como X ve (1,2,3)
+				# Depois, aplicamos [X,Y] e com isso temos como Y ve (1,2,3)
+				# E isso é como transformamos o de 0 em Y, que é [0,Y]
+				
+				f3 = permutacoes[(0, par[0])]
+				f4 = permutacoes[par]
+				funcaoComposta2 = comporFuncao(f4,f3) 
+				#print('_______COMPONDO FUNCOES______')
+				#print ('f1(1,2,3):', f3(1,2,3), 'par',(par[0], 0) )
+				#print ('f2(1,2,3):', f4(1,2,3), 'par',(par[1],par[0]))
+				#print ('fc(1,2,3):', funcaoComposta2(1,2,3), 'par',(0,par[1]) )
+				#print('_____________________________')
+				novasPermutacoes[novoPar] = funcaoComposta2
 				
 		posicoesRelativas.update(novosCaminhos)
 		permutacoes.update(novasPermutacoes)
@@ -177,80 +186,42 @@ while ( len([x for x,y in posicoesRelativas.items() if x[0]==0] ) < qtdScanners)
 		#[print(x,y) for x,y in posicoesRelativas.items()]
 		#print('___________')
 #[print(x,y) for x,y in posicoesRelativas.items()]
-[print(x,y) for x,y in posicoesRelativas.items() if x[0] == 0]
-print('___')
-[print(x) for x in permutacoes]
+#[print(x,y) for x,y in posicoesRelativas.items() if x[0] == 0]
+#print('___')
+#[print(x) for x in permutacoes]
 beaconsTotaisReferentesAoZero = set()
+
 for indice, lista in enumerate(listaScanners):
 	funcaoParaEsseScanner = permutacoes[(0, indice)]
-	print(lista)
-	input()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		#print(novosCaminhos)
-		#input()
-#		posicoesRelativas.update(novosCaminhos)
-#		[print(x,y) for x,y in posicoesRelativas.items()]
-#		input()
+	parIndice = (0,indice)
+	#print('comecando par', parIndice)
+	distanciaAoZero = posicoesRelativas[parIndice]
+	funcaoDeTransformacao = permutacoes[(parIndice[1],parIndice[0])]
 	
-	#caminhosDescobertosAteOZero = {x:y for x,y in posicoesRelativas.items() if x[0] == 0}
-#	for rota, distancia in caminhosDescobertosAteOZero.items():
-#		destino = rota[1]
-#		novosCaminhos = {(0,x[0]):
-#						
-#						diminuiTuplas(
-#							permutacoesInversasDeUmaTupla(permutacoes[(0,x[1])])(*y),
-#							distancia
-#						) 
-#						for x,y in posicoesRelativas.items() if x[1] == destino # }
-#															and (0, x[0]) not in posicoesRelativas}
-		#print('novosembaixo', novosCaminhos)
-		#print(novosCaminhos)
-#		posicoesRelativas.update(novosCaminhos)
-#		[print(x,y) for x,y in posicoesRelativas.items()]
-#		input()
-#	print('PosicoesRelativas:')
-#	[print(x,y) for x,y in posicoesRelativas.items()]
-#	input()
+	listaTransformada = [funcaoDeTransformacao(*x) for x in lista]
+	listaTransladada = [somaTuplas(x, distanciaAoZero) for x in listaTransformada]
 
-caminhosDescobertosAteOZero = {x:y for x,y in posicoesRelativas.items() if x[0] == 0}
-#[print(x,y) for x,y in caminhosDescobertosAteOZero.items()]
-#(0, 1) (68, -1246, -43)
-#(1, 3) (160, -1134, -23)
-#(2, 4) (1125, -168, 72)
-#(1, 4) (88, 113, -1104)
+	
+	#[print(listaScanners[0][i],'\t\t', lista[i],'\t\t', listaTransladada[i] )  for i in range(25)]
+	beaconsTotaisReferentesAoZero.update(listaTransladada)
+print(len(beaconsTotaisReferentesAoZero))
+
+
+caminhosDescobertosAteOZero = [y for x,y in posicoesRelativas.items() if x[0] == 0]
+
+
+maiorDistancia = 0
+
+for indice1 in range(len(caminhosDescobertosAteOZero)):
+	for indice2 in range(indice1 + 1, len(caminhosDescobertosAteOZero)):
+		#Calcular distancia entre esses dois:
+		d1 = caminhosDescobertosAteOZero[indice1]
+		d2 = caminhosDescobertosAteOZero[indice2]
+		
+		dist = abs(d1[0] - d2[0]) + abs(d1[1] - d2[1]) + abs(d1[2] - d2[2])
+		maiorDistancia = max(maiorDistancia, dist)
+		
+print(maiorDistancia)
 
 
 
-
-#gabarito = {0:(0,0,0)} # Dicionario que relaciona cada scanner ao scanner 0.
-#while(len(listaScanners) != len(gabarito)):
-#	itemsAAdicionar = {}
-#	for x,y in gabarito.items():
-#		novoCaminho={(x,x2[1]):somaTuplas(y,y2) for x2,y2 in posicoesRelativas.items() if x==x2[0] and x2[1] not in gabarito}
-#		print('NovoCaminho', novoCaminho)
-#		input()
-#		posicoesRelativas.update(novoCaminho) 
-#		posicoesAoZero = {x[1]:y for x,y in posicoesRelativas.items() if x[0] == 0}
-#		itemsAAdicionar.update(posicoesAoZero)
-#		print('itemsAAdicionar', itemsAAdicionar) 
-#		input()
-#	gabarito.update(itemsAAdicionar)
-#	print(gabarito)
-#	input()
-#(0, 1) (68, -1246, -43)
-#(1, 3) (160, -1134, -23)
-#(2, 4) (1125, -168, 72)
-#(1, 4) (88, 113, -1104)
