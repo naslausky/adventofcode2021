3244dcfea8c160993ce130478baa4cab875b4a5f
Eduardo Naslausky <eduardonaslausky@hotmail.com>
2022-10-24 02:14:27 -0300

    Adiciona mais algumas correções e comentários ao vigésimo quarto dia.

--- 24/24.py
+++ 24/24.py
@@ -11,7 +11,7 @@ with open('input.txt') as file:
 	for instrucao in instrucoes:
 		comando = instrucao.split()[0]
 		if comando == 'inp' :
-			if blocoAtual: # Só precisa disso para a primeira linha.
+			if blocoAtual: # Para a primeira linha sendo um input não adicionar um bloco vazio.
 				listaBlocosInstrucoes.append(blocoAtual)
 				blocoAtual = []
 		else:
@@ -19,14 +19,14 @@ with open('input.txt') as file:
 			blocoAtual.append((comando, destino, origem))
 	listaBlocosInstrucoes.append(blocoAtual)
 
-def seguirInstrucoes(listaInstrucoes, ZAnterior, WDigitado): # Função que retorna o valor do registrador Z após seguir o bloco de instrucoes dado.
-	registradores = {'w' : WDigitado, 'x' : 0, 'y' : 0, 'z' : ZAnterior}
-	# Após analisar meu input verifiquei que a cada bloco de instruções o Y e o X não fazem diferença. 
+def seguirInstrucoes(listaInstrucoes, ZAnterior, WDigitado): # Função que retorna o valor do registrador Z após seguir o bloco de instruções dado e o W inserido.
+	registradores = {'x' : 0, 'y' : 0, 'z' : ZAnterior, 'w' : WDigitado}
+	# Após analisar meu input verifiquei que a cada bloco de instruções o Y e o X não fazem diferença para cada bloco de instruções. 
 	for instrucao in listaInstrucoes:
 		comando, destino, origem = instrucao
 		valorDestino = registradores[destino]
 
-		if origem in 'wxyz':
+		if origem in 'wxyz': # Verifica se a origem é um número fixo ou o oriundo de um registrador.
 			valorOrigem = registradores[origem]
 		else:
 			valorOrigem = int(origem)
@@ -52,12 +52,13 @@ def seguirInstrucoes(listaInstrucoes, ZAnterior, WDigitado): # Função que reto
 	return registradores['z']
 
 valoresDeZASeremTestados = [(0, 0, 0)] 
-# Lista de valores de Z. Cada elemento é uma tupla que contém o valor atual de Z e os números digitados para obtê-lo.
-# Essa implementação é uma brute-force que se baseia na descoberta que todos os blocos de códigos não utilizam os valores anteriores de X Y ou W. Pela quantidade de números salvos, requer bastante tempo e memória (~25 min e um máximo de 2,5GB)
+# Lista de valores de Z. Cada elemento é uma tupla que contém o valor atual de Z e os (maior e menor) números digitados para obtê-lo.
+# Essa implementação é uma brute-force que se baseia na descoberta que todos os blocos de códigos não utilizam os valores anteriores de X, Y ou W. Pela quantidade de números salvos, requer bastante tempo e memória (~25 min e um máximo de 2,5GB)
 for blocoInstrucoes in listaBlocosInstrucoes:
-	proximosValoresDeZ = {} # Dicionário que relaciona os valores de Z a tupla que representa ele.
+	proximosValoresDeZ = {} # Dicionário que relaciona os valores de Z a tupla que representa ele. 
+	# Usado para caso dois valores gerem o mesmo valor Z, salvar só os com menor e maior códigos.
 	for zAnterior, maiorDigitadoAteAgora, menorDigitadoAteAgora in valoresDeZASeremTestados:
-		for proximoDigito in range(1, 10):
+		for proximoDigito in range(1, 10): # Para cada possibilidade de dígito a ser inserido.
 			ZSeguinte = seguirInstrucoes(blocoInstrucoes, zAnterior, proximoDigito)
 			maiorDigitadoSeguinte = maiorDigitadoAteAgora * 10 + proximoDigito
 			menorDigitadoSeguinte = menorDigitadoAteAgora * 10 + proximoDigito
@@ -66,6 +67,6 @@ for blocoInstrucoes in listaBlocosInstrucoes:
 											max(maiorValorConhecido, maiorDigitadoSeguinte),
 											min(menorValorConhecido, menorDigitadoSeguinte))
 	valoresDeZASeremTestados = list(proximosValoresDeZ.values())
-resposta = [x for x in valoresDeZASeremTestados if x[0] == 0][0]
+resposta = [tuplaCodigos for tuplaCodigos in valoresDeZASeremTestados if x[0] == 0][0]
 print('O maior valor válido a ser inserido é:', resposta[1])
 print('O menor valor válido a ser inserido é:', resposta[2])
